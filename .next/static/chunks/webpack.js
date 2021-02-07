559a01facf5799cb73adfa1f9174fc2a84d5b0f4
endymion1818 <endymion1818@gmail.com>
2021-02-07 17:34:55 +0000

    fix imports

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9ba2876b8659ccc1b0a9";
+/******/ 	var hotCurrentHash = "35c83600a61311c3080d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/HandwritingPractice/HandwritingPractice.tsx
+++ src/components/HandwritingPractice/HandwritingPractice.tsx
@@ -1,26 +1,58 @@
 import React from 'react'
+import './stylesheet.module.css'
 
 const HandwritingPractice = () => {
+
+    const [message, useMessage] = React.useState('Edit me!')
+    const font = 'KG Primary Dots'
+    const scale = 'large'
+    const textAlign = 'left'
+    const showLines = true
+    let lines = []
+    
+    const linesList = function () {
+        lines = message
+            .split("\n")
+            .map(l => l.trim())
+    }
+    const lineStyle = function () {
+        return {
+            fontSize: scale === 'large' ?  `152px` : `40px`,
+            fontFamily: font,
+            lineHeight: scale === 'large' ?  `1.3` : `1.2`,
+        }
+    }
+    const printStyle = function () {
+        return {
+            textAlign: textAlign,
+            paddingTop: scale === 'large' ? `40px` : `72px`
+        }
+    }
+    const printHandler:React.MouseEvent<HTMLButtonElement> | React.KeyboardEvent<HTMLButtonElement> = function() {
+        if (typeof window !== 'undefined') {
+            window.print
+        }
+    }
     return (
-        <div>
+        <div className="container">
             <div id="edit">
-            <textarea v-model="message"></textarea>
+            <textarea value={message} onChange={e => useMessage(e.currentTarget.value)} />
         </div>
             <div id="font">
                 <fieldset>
                     <label htmlFor="dot">Font: </label>
-                    <select v-model="font">
+                    <select>
                     <option id="dot" value="KG Primary Dots" selected>Primary</option>
                     <option id="dot-line-cursive" value="Learning Curve Dashed" >Dots lined Cursive</option>
                 </select>
                 </fieldset>
                 <fieldset>
                     <label htmlFor="lines">Show lines: </label>
-                    <input type="checkbox" v-model="showLines"/>
+                    <input type="checkbox" checked={showLines} />
                 </fieldset>
                 <fieldset>
                     <label htmlFor="align">Align: </label>
-                    <select id="align" v-model="textAlign">
+                    <select id="align">
                         <option id="to-left" value="left" selected>Left</option>
                         <option id="to-center" value="center">Center</option>
                         <option id="to-right" value="right">Right</option>
@@ -28,13 +60,13 @@ const HandwritingPractice = () => {
                 </fieldset>
                 <fieldset>
                     <label htmlFor="scale">Font Size</label>
-                    <select id="scale" v-model="scale">
+                    <select id="scale" value={scale} onChange={printStyle}>
                         <option id="large" value="large" selected>Large</option>
                         <option id="small" value="small">Small</option>
                     </select>
                 </fieldset>
                 <fieldset>
-                    {/* <button onClick={window.print()}>Print</button> */}
+                    <button onClick={printHandler}>Print</button>
                 </fieldset>
             </div>
             <div id="printarea">
@@ -42,11 +74,13 @@ const HandwritingPractice = () => {
                     <p>Student name: ______________________</p>
                     <p>Date: ___________</p>
                 </section>
-                <p className="scale" v-for="line in lines" >
+                {lines && lines.map((line, index) => (
+                    <p key={index} className="scale" v-for="line in lines" >
                     <img src="./images/handwriting-singleline-large.png" alt="lined background " v-if="showLines === true && scale === 'large'"/>
                     <img src="./images/handwriting-singleline-small.png" alt="lined background " v-if="showLines === true && scale === 'small'" />
-                    {/* {{ line }} */}
+                    {line}
                 </p>
+                ))}
             </div>
         </div>
     )
