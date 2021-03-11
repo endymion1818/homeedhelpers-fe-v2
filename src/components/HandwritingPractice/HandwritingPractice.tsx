8b626142b91063e3219149db4ecbdbe5cdc250c5
endymion1818 <endymion1818@gmail.com>
2021-03-11 14:17:04 +0000

    move content to page from component

--- src/components/HandwritingPractice/HandwritingPractice.tsx
+++ src/components/HandwritingPractice/HandwritingPractice.tsx
@@ -5,11 +5,11 @@ import clsx from 'clsx'
 const lineScales = [
     {
         name: 'large',
-        value: '152px'
+        value: 'large'
     },
     {
         name: 'small',
-        value: '40px'
+        value: 'small'
     }
 ]
 const fontStyles = [
@@ -28,8 +28,8 @@ const alignmentStyles = [
         value: 'left'
     },
     {
-        name: 'center',
-        value: 'center'
+        name: 'centre',
+        value: 'centre'
     },
     {
         name: 'right',
@@ -68,71 +68,41 @@ const HandwritingPractice = () => {
     )
 
     return (
-        <div className="container">
-            <h1>Handwriting practice paper</h1>
-            <p>Make full-page custom handwriting worksheets in seconds!</p>
-            <ol className={styles.instructions}>
-                <li>Select a writing style: <i>Primary (d'nealian)</i> or <i>cursive</i></li>
-                <li>Type your handwriting worksheet content and test how you want it to appear</li>
-                <li>Select your letter size</li>
-                <li>Click <i>Print</i> for a copy of your writing worksheet for immediate use
-                </li>
-            </ol>
-            <p id={styles.sorrynotresponsive} data-nosnippet>Sorry! You'll need to use a device with a larger screen for this to work.</p>
-            <div id={styles.font}>
-                <fieldset className={styles.fieldset}>
-                    <label htmlFor="dot">Font: </label>
-                    <Select values={fontStyles} callback={setFont} selected={fontStyles[0]} />
-                </fieldset>
-                <fieldset className={styles.fieldset}>
-                    <label htmlFor="lines">Show lines: </label>
-                    <input 
-                        id="lines" 
-                        type="checkbox" 
-                        defaultChecked={true} 
-                        onChange={() => setShowLines(!showLines)} 
-                    />
-                </fieldset>
-                <fieldset className={styles.fieldset}>
-                    <label htmlFor="align">Align: </label>
-                    <Select values={alignmentStyles} callback={setTextAlign} selected={alignmentStyles[0]} />
-                </fieldset>
-                <fieldset className={styles.fieldset}>
-                    <label htmlFor="scale">Font Size</label>
-                    <Select values={lineScales} callback={setScale} selected={lineScales[0]} />
-                </fieldset>
-                <fieldset className={styles.fieldset}>
-                    <button onClick={() => window.print}>Print</button>
-                </fieldset>
-            </div>
-            <div id={styles.printarea}>
-                <section id={styles.printarea__header}>
-                    <p>Student name: ______________________</p>
-                    <p>Date: ___________</p>
-                </section>
-                <p className={classes} contentEditable="true">Edit me!</p>
-            </div>
-            <hr/>
-        <h2>Frequently asked questions</h2>
-        <ol className={styles.instructions}>
-            <li>
-                <strong>What is this?</strong>
-                <i>This is a tool to help teachers and home educators give their students and children custom handwriting worksheets learn how to write.</i>
-            </li>
-            <li>
-                <strong>How do I use it?</strong>
-                <i>Enter the text you want your student or child to write into the text box provided. Click the "print" button, and your printer dialog will appear. We have configured the print version to contain an area for the student to write their name and the date on the printed sheet.</i>
-            </li>
-            <li>
-                <strong>Is there a charge for this service?</strong>
-                <i>No. This service is provided free of charge. We hope to make it pay for itself by the use of advertising in the future.</i>
-            </li>
-            <li>
-                <strong>How do I contact you?</strong>
-                <i>You can email homeedhelpers@gmail.com to send feature suggestions and bug reports. We will read your email but cannot guarantee a response.</i>
-            </li>
-        </ol>
-      </div>
+        <>
+        <div id={styles.font}>
+            <fieldset className={styles.fieldset}>
+                <label htmlFor="dot">Font: </label>
+                <Select values={fontStyles} callback={setFont} selected={fontStyles[0]} />
+            </fieldset>
+            <fieldset className={styles.fieldset}>
+                <label htmlFor="lines">Show lines: </label>
+                <input 
+                    id="lines" 
+                    type="checkbox" 
+                    defaultChecked={true} 
+                    onChange={() => setShowLines(!showLines)} 
+                />
+            </fieldset>
+            <fieldset className={styles.fieldset}>
+                <label htmlFor="align">Align: </label>
+                <Select values={alignmentStyles} callback={setTextAlign} selected={alignmentStyles[0]} />
+            </fieldset>
+            <fieldset className={styles.fieldset}>
+                <label htmlFor="scale">Font Size: </label>
+                <Select values={lineScales} callback={setScale} selected={lineScales[0]} />
+            </fieldset>
+            <fieldset className={styles.fieldset}>
+                <button onClick={() => window.print}>Print</button>
+            </fieldset>
+        </div>
+        <div id={styles.printarea}>
+            <section id={styles.printarea__header}>
+                <p>Student name: ______________________</p>
+                <p>Date: ___________</p>
+            </section>
+            <p className={classes} contentEditable="true">Edit me!</p>
+        </div>
+      </>
     )
 }
 
--- src/pages/handwriting-practice-paper.tsx
+++ src/pages/handwriting-practice-paper.tsx
@@ -11,7 +11,56 @@ const Index = () => {
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
-        <HandwritingPractice />
+        <div className="container">
+          <h1>Handwriting practice paper</h1>
+            <p>Make full-page custom handwriting worksheets in seconds!</p>
+            <ol className="instructions">
+                <li>Select a writing style: <i>Primary (d'nealian)</i> or <i>cursive</i></li>
+                <li>Type your handwriting worksheet content and test how you want it to appear</li>
+                <li>Select your letter size</li>
+                <li>Click <i>Print</i> for a copy of your writing worksheet for immediate use
+                </li>
+            </ol>
+            <p id="sorrynotresponsive" data-nosnippet>Sorry! You'll need to use a device with a larger screen for this to work.</p>
+          <HandwritingPractice />
+          <h2>Frequently asked questions</h2>
+          <ol className="instructions">
+              <li>
+                  <strong>What is this?</strong>
+                  <i>This is a tool to help teachers and home educators give their students and children custom handwriting worksheets learn how to write.</i>
+              </li>
+              <li>
+                  <strong>How do I use it?</strong>
+                  <i>Enter the text you want your student or child to write into the text box provided. Click the "print" button, and your printer dialog will appear. We have configured the print version to contain an area for the student to write their name and the date on the printed sheet.</i>
+              </li>
+              <li>
+                  <strong>Is there a charge for this service?</strong>
+                  <i>No. This service is provided free of charge. We hope to make it pay for itself by the use of advertising in the future.</i>
+              </li>
+              <li>
+                  <strong>How do I contact you?</strong>
+                  <i>You can email homeedhelpers@gmail.com to send feature suggestions and bug reports. We will read your email but cannot guarantee a response.</i>
+              </li>
+          </ol>
+        </div>
+        <style jsx>{`
+          .instructions {
+            padding-inline-start: 20px;
+          }
+          .instructions > li {
+              line-height: 1.5;
+              margin-bottom: 5px;
+          }
+          #sorrynotresponsive {
+            display: none;
+          }
+          @media (max-width: 550px) {
+              #sorrynotresponsive {
+                  display: block;
+              }
+          }
+        `}
+        </style>
       </Layout>
   )
 }
