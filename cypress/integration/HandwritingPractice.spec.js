1dbfb7aef77e7c0598e1904f3d3eb61f120d1fb6
endymion1818 <endymion1818@gmail.com>
2021-03-12 17:06:09 +0000

    prettify files

--- cypress/integration/HandwritingPractice.spec.js
+++ cypress/integration/HandwritingPractice.spec.js
@@ -1,13 +1,13 @@
 describe("<HandwritingPractice />", () => {
-    beforeEach(() => {
-        cy.visit('/handwriting-practice-paper')
-    })
-    
-    describe("When you visit the handwriting practice page", () => {
-      it("Should allow you to type a new message into the print area", () => {
-        const message = "new message";
-        cy.get("[data-cy=textarea]").type(message);
-        cy.get("[data-cy=printarea]").contains(message);
-      });
+  beforeEach(() => {
+    cy.visit("/handwriting-practice-paper");
+  });
+
+  describe("When you visit the handwriting practice page", () => {
+    it("Should allow you to type a new message into the print area", () => {
+      const message = "new message";
+      cy.get("[data-cy=textarea]").type(message);
+      cy.get("[data-cy=printarea]").contains(message);
     });
+  });
 });
